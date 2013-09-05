e2253980b16af964f312751dc862f1aeabb939f2
k3_kaimu <enjouzensyou.boinc@gmail.com>
2013-09-05 14:50:49 +0900

    util.numeric.dの追加

---
+++ util/numeric.d
@@ -0,0 +1,17 @@
+module util.numeric;
+
+import std.exception;
+import std.math;
+import std.traits;
+
+F isValidNum(F)(F f) pure nothrow @safe @property
+{
+    return !(isNaN(f) || isInfinity(f));
+}
+
+
+auto ref F enforceValidNum(F)(auto ref F f,  string file = __FILE__, size_t line = __LINE__) pure @safe
+{
+    enforce(f.isValidNum, "argument is NaN or infinite", file, line);
+    return f;
+}
